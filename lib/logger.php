f46591d9d12595f0173cb5cc6a5f02e2df9eac49
Gavin Brown <gavin.brown@fastmail.uk>
2025-11-13 13:32:51 +0000

    tabs to spaces

--- lib/logger.php
+++ lib/logger.php
@@ -3,16 +3,16 @@
 namespace rdap_org;
 
 class logger {
-	private static mixed $STDOUT = null;
+    private static mixed $STDOUT = null;
     private static mixed $REDIS  = null;
 
-	public static function logRequest(
-		\OpenSwoole\HTTP\Request 	$request,
-		int 											$status,
-		ip 												$peer,
-	) : void {
+    public static function logRequest(
+        \OpenSwoole\HTTP\Request     $request,
+        int                                             $status,
+        ip                                                 $peer,
+    ) : void {
 
-		if (is_null(self::$STDOUT)) self::$STDOUT = fopen('php://stdout', 'w');
+        if (is_null(self::$STDOUT)) self::$STDOUT = fopen('php://stdout', 'w');
 
         if (is_null(self::$REDIS)) {
             $host = getenv("REDIS_HOSTNAME");
@@ -54,17 +54,17 @@ class logger {
             }
         }
 
-		fprintf(
-			self::$STDOUT,
-			"%s - - [%s] \"%s %s %s\" %03u 0 \"%s\" \"%s\"\n",
-			strval($peer),
-			gmdate('d/m/Y:h:i:s O'),
-			$request->server['request_method'],
-			$request->server['request_uri'],
-			$request->server['server_protocol'],
-			$status,
-			$request->header['referer'] ?? '-', 
-			$request->header['user-agent'] ?? '-', 
-		);
-	}
+        fprintf(
+            self::$STDOUT,
+            "%s - - [%s] \"%s %s %s\" %03u 0 \"%s\" \"%s\"\n",
+            strval($peer),
+            gmdate('d/m/Y:h:i:s O'),
+            $request->server['request_method'],
+            $request->server['request_uri'],
+            $request->server['server_protocol'],
+            $status,
+            $request->header['referer'] ?? '-', 
+            $request->header['user-agent'] ?? '-', 
+        );
+    }
 }
