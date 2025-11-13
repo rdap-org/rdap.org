5ac5a3aa7a9ed7d079ccfa769657f0f02150cda4
Gavin Brown <gavin.brown@fastmail.uk>
2025-11-13 13:32:11 +0000

    maintain connection to the Redis server and use it to track query volumes

--- lib/logger.php
+++ lib/logger.php
@@ -14,6 +14,46 @@ class logger {
 
 		if (is_null(self::$STDOUT)) self::$STDOUT = fopen('php://stdout', 'w');
 
+        if (is_null(self::$REDIS)) {
+            $host = getenv("REDIS_HOSTNAME");
+
+            if (is_string($host) && strlen($host) > 0) {
+                try {
+                    self::$REDIS = new Redis();
+
+                    self::$REDIS->connect(
+                        $host,
+                        6379,
+                        1,
+                        "",
+                        0,
+                        0,
+                        ["auth" => [getenv("REDIS_USERNAME"), getenv("REDIS_PASSWORD")]]
+                    );
+
+                } catch (\Throwable $e) {
+                    self::$REDIS = null;
+
+                    error_log(sprintf("Unable to connect to Redis server on %s: %s", $host, $e->getMessage()));
+                }
+            }
+        }
+
+        if (!is_null(self::$REDIS)) {
+            if (!self::$REDIS->isConnected()) {
+                self::$REDIS = null;
+
+            } else {
+                try {
+                    self::$REDIS->incr("total_queries");
+
+                } catch (\Throwable $e) {
+                    error_log($e->getMessage());
+
+                }
+            }
+        }
+
 		fprintf(
 			self::$STDOUT,
 			"%s - - [%s] \"%s %s %s\" %03u 0 \"%s\" \"%s\"\n",
